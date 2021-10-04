662114074c5f7fea8b098eda849fad6d496c6083
Krzysztof Tworus <ktworus@gmail.com>
2021-10-04 13:23:36 +0200

    10_04

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Krzysiek"); // [3]
+        System.out.println(44); // [3]
+        System.out.println('T'); // [3]
+
+    }
+}
