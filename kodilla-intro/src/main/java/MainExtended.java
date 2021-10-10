40fe518aca5600158212e22583030806407c540c
Krzysztof Tworus <ktworus@gmail.com>
2021-10-10 09:06:47 +0200

    10_10

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String anyName = "John";
+        int somebodysAge = 32;
+        double decimalValue = 22.22;
+        char firstLetter = 'A';
+        boolean isAvailable = true;
+
+        System.out.println(somebodysAge);
+        System.out.println(anyName);
+        System.out.println(decimalValue);
+        System.out.println(firstLetter);
+        System.out.println(isAvailable);
+    }
+}
