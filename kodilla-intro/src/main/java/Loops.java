adc07de10026847ecc8af259da344c7642522ff4
Krzysztof Tworus <ktworus@gmail.com>
2021-10-11 21:29:33 +0200

    11_10

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,25 +1,21 @@
 public class Loops {
     public static void main(String[] args) {
 
-        int[] numbers = new int[]{5, 20, 52};
-
+        int[] numbers = new int[]{5, 2, 52};
+        int result = sumAndDisplay(numbers);
+        System.out.println(result);
+    }
+    public static int sumAndDisplay(int[] numbers) {
         int sum = 0;
 
-
-
-
-
         for (int i = 0; i < numbers.length; i++) {
-         int num = numbers[i];
-
+            int num = numbers[i];
             sum = sum + num;
-
-
+        }
+            return sum;
         }
 
-        System.out.println(sum);
-
-
-        }
 
     }
+
+
