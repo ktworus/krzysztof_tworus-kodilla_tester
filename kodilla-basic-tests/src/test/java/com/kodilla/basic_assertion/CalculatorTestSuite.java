2216e0ddeb31ee01491f0531bbc73d9832675aca
Krzysztof Tworus <ktworus@gmail.com>
2021-10-30 12:15:52 +0200

    s

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,12 @@
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+@Test
+public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+        }
