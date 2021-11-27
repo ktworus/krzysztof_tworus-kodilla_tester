663a76eda934efb48ec6f73d385799fe93263e44
Krzysztof Tworus <ktworus@gmail.com>
2021-11-27 15:52:57 +0100

    gambling

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GamblingMachineTestSuite {
+    GamblingMachine gamblingMachine = new GamblingMachine();
+    Set<Integer> testNumbers = new HashSet<>();
+    @ParameterizedTest
+    @CsvFileSource(resources = "/GamblingMachine.csv", numLinesToSkip = 1)
+
+    public void toCheckInvalidNumbersException(){
+
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(testNumbers));
+
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3, 6, 3, 12, 15})
+    public void toCheckNumberOfWins(Set<Integer> testNumbers, int expected) throws InvalidNumbersException {
+        assertEquals(expected, gamblingMachine.howManyWins(testNumbers));
+    }
+
+
+}
