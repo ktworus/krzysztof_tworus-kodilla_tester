114ff0dfeb9976ef9198338efdc5bb5e8363cc71
Krzysztof Tworus <ktworus@gmail.com>
2022-01-29 21:26:37 +0100

    aa

--- kodilla-cucumber/src/test/java/seasonchecker/IsItSummerSteps.java
+++ kodilla-cucumber/src/test/java/seasonchecker/IsItSummerSteps.java
@@ -13,8 +13,37 @@ public class IsItSummerSteps implements En {
         Given("today is last day of December", () -> {
             this.today = MonthDay.of(12,31);
         });
+
         Given("today is first day of August", () -> {
             this.today = MonthDay.of(9, 1);
+        });
+
+        Given("today is second day of January", () -> {
+            this.today = MonthDay.of(1, 2);
+        });
+
+        Given("today is 10th day of February", () -> {
+            this.today = MonthDay.of(2, 10);
+        });
+
+        Given("today is 20th day of June", () -> {
+            this.today = MonthDay.of(6, 20);
+        });
+
+        Given("today is 5th day of May", () -> {
+            this.today = MonthDay.of(5, 5);
+        });
+
+        Given("today is 24th day of September", () -> {
+            this.today = MonthDay.of(9, 24);
+        });
+
+        Given("today is 21th day of June", () -> {
+            this.today = MonthDay.of(6, 21);
+        });
+
+        Given("today is 23th day of August", () -> {
+            this.today = MonthDay.of(8, 24);
         });
 
         When("I ask whether it's Summer", () -> {
@@ -24,7 +53,6 @@ public class IsItSummerSteps implements En {
 
         Then("I should be told {string}", (String string) -> {
             Assert.assertEquals(string, this.answer);
-
         });
     }
 }
